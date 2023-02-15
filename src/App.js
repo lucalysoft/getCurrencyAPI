d61ee59b389faef96e6aad65d60aa0acb6e25b18
luclay <luclay17@gmail.com>
2023-02-14 20:48:45 -0800

    ui update

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ function App() {
   const [dcp, setDcp] = useState(0.0); 
   const [result, setResult] = useState(0.0);  
   const [curValue, setCurValue] = useState(0.0); 
-  const [dcpflag, setDcpflag] = useState(false)
+  const [dcpflag, setDcpflag] = useState(false);
+  const [listData, setListData] = useState([]); 
   useEffect(() => {
     getCurrentResult();
-    // 
   }, []);
-  const [listData, setListData] = useState([]); 
+  
   
   const getDCP = () => new Promise((resolve, reject) => {
     axios.get('https://metrics.router.democraticcooperative.cash/metrics/last')
@@ -46,7 +46,7 @@ function App() {
     await getDCP();
     await getTicker();
     if(listData.length>0)setCurValue(listData[0].value);
-    else setCurValue(0)
+    // else setCurValue(0)
   }
   function showCurrency(e){
     setCurValue(listData.filter(item => item.curtype === e.target.value)[0].value);    
@@ -67,31 +67,41 @@ function App() {
   }
   
   return (
-        <div className=' w-full h-full justify-center items-center'>
-          <div className='flex flex-col'>
-            <div className='flex flex-row mt-20px space-x-5'>
-              <p>              
-                DCP:&nbsp; &nbsp;</p> 
-                {!dcpflag?(<input type="text" className="w-44" value={dcp} onChange={(e) => setDcp(e.target.value)}></input>):(<input type="text" className="w-44" value={dcp}></input>)}
-              <p>
-                ConvertTo:&nbsp; &nbsp;<select name="cars" id="cars" onChange={showCurrency}> 
+        <div className='bg-black  h-full items-center'>
+          <div className='flex flex-col items-center'>
+          <p className='text-white font-black text-7xl  mt-56 items-center'>Facinating converter</p>
+            <div className='mt-12 flex flex-col mt-20px'>
+              <p className='mt-12  text-white text-3xl'>              
+                <select name="cars" id="cars" className="fixed rounded-xl hover:bg-sky-700 w-36 h-16 bg-indigo-500" >
+                  <option id="a" value = "Satosh">Satosh</option>
+                  <option id="a" value = "Satosh">Bitcoin</option>
+                </select>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                {!dcpflag?(<input type="text" className="rounded-lg text-center text-3xl h-16 font-semibold bg-slate-700 text-white w-96" value={dcp} onChange={(e) => setDcp(e.target.value)}></input>):(<input type="text" className=" rounded-lg w-96 text-center font-semibold bg-slate-700 text-3xl h-16 " value={dcp}></input>)}
+              </p>
+              
+              <p className='mt-12 text-white text-3xl'>
+                <select name="cars" id="cars" className="fixed rounded-xl hover:bg-sky-700 w-36 h-16 bg-indigo-500" onChange={showCurrency}> 
                             {listData.map((menuItem, index) => (
                               <option id="x" key={index} value={menuItem.curtype}>{menuItem.curtype}</option>
                             ))}
                           </select> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                <span>{curValue}</span>          
+                {/* <span>{curValue}</span>  */}
+                <input type="text" className="inline-block rounded-lg h-16 text-center bg-slate-700 text-white w-96" value={curValue} onChange={(e) => setCurValue(e.target.value)}></input>         
               </p>  
               
             </div>
-            <div className='flex flex-row mt-3'>
-              <button className="bg-indigo-500 opacity-100 ..." onClick={switchSelection}>switch selection</button> 
-              &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-              <button className="bg-indigo-500 opacity-100 ..." onClick={showConvertValue}>convert</button>   
+            <div className='mt-12 flex flex-row'>
+              {/* <button className="bg-indigo-500 text-center hover:bg-sky-700 font-semibold rounded-lg text-3xl text-white " onClick={switchSelection}>switch selection</button> 
+              &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; */}
+              {/* <button className="bg-indigo-500  text-center rounded-lg font-semibold hover:bg-sky-700 shadow-slate-100 w-36 h-16 text-3xl text-white " onClick={showConvertValue}>convert</button>  
+              <button  className="bg-indigo-500"></button> */}
+              <img width="60" height="40" src="updown.png" className='rounded-lg hover:bg-white mb-40' alt="animal"></img>
             </div>
             
-            <p className=' mt-3'>
+            {/* <p className='text-white mt-3 text-3xl'>
               Current DCP is:&nbsp; &nbsp;{result}
-            </p>
+            </p> */}
+            <p className='text-black absolute bottom-0 text-3xl'>Copyright(2023): This software will be secured by authors.</p>
           </div>
         </div>
      
